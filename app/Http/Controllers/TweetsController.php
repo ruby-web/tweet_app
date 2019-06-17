7e65bf0721df2af16606dc7a12461e106b208a69
ruby-web <rubystudy190220@gmail.com>
2019-06-17 21:16:13 +0900

    first commit

---
+++ app/Http/Controllers/TweetsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Tweet;
+
+class TweetsController extends Controller
+{
+
+  public function __construct()
+  {
+      $this->middleware('auth', array('except' => 'index'));
+  }
+  
+    public function index()
+  {
+    $tweets = Tweet::all();
+
+    $tweets = Tweet::orderBy('created_at', 'DESC')->paginate(5);
+    return view('tweets.index')->with('tweets', $tweets);
+  }
+
+    public function create()
+    {
+        return view('tweets.create');
+    }
+
+    public function store(Request $request)
+  {
+    Tweet::create(
+      array(
+        'name' => $request->name,
+        'image' => $request->image,
+        'text' => $request->text
+      )
+    );
+
+    
+    return view('tweets.store');
+  }
+}
---
+++ resources/views/tweets/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('content')
+<div class="contents row">
+  @foreach($tweets as $tweet)
+    <div class="content_post" style="background-image: url({{ $tweet->image }});">
+      <p>{{ $tweet->text }}</p>
+      <span class="name">{{ $tweet->name }}</span>
+    </div>
+  @endforeach
+  {{ $tweets->render() }}
+</div>
+@endsection
